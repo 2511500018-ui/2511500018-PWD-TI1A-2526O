f94623dc414f2f883522cb9fd09ffe76c2d7c9bb
unknown <2511500018@mahasiswa.atmaluhur.ac.id>
2025-10-22 11:29:38 +0700

    tombol hamburger

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,3 +1,4 @@
 document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
 })
+alert("Halo Dunia dari file exsternal");
