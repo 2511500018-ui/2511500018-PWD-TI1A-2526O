6d42f53806b2ef4a303ae74d094d80af218b0170
unknown <2511500018@mahasiswa.atmaluhur.ac.id>
2025-10-23 16:39:13 +0700

    1

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -1,5 +1,1 @@
-document.getElementById("menuToggle").addEventListener("click", function () {
-    document.querySelector("nav").classList.toggle("active");
-})
-alert("Halo Dunia dari file exsternal");
 
