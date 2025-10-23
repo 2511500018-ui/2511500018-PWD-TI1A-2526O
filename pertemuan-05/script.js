287c5ec0524fab28f999edbc2872da5be54041e6
unknown <2511500018@mahasiswa.atmaluhur.ac.id>
2025-10-23 16:11:57 +0700

    toombol

--- pertemuan-05/script.js
+++ pertemuan-05/script.js
@@ -2,3 +2,5 @@ document.getElementById("menuToggle").addEventListener("click", function () {
     document.querySelector("nav").classList.toggle("active");
 })
 alert("Halo Dunia dari file exsternal");
+
+dokument.querySelector
