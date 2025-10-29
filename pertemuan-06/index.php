f6a90ebe2ec02075f818ec5fe05d9dbe44cf7a84
unknown <2511500018@mahasiswa.atmaluhur.ac.id>
2025-10-29 11:57:08 +0700

    hello.php

---
+++ pertemuan-06/index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Judul Halaman</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header>
+    <h1>Ini Header</h1>
+    <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigation">
+      &#9776; 
+    </button>
+    <nav>
+      <ul type="square">
+        <li><a href="#home">Beranda</a></li>
+        <li><a href="#about">Tentang</a></li>
+        <li><a href="#contact">Kontak</a></li>
+      </ul>
+    </nav>
+  </header>
+ 
+  <main>
+    <section id="home">
+      <h2>Selamat Datang</h2>
+      <p>Ini contoh paragraf HTML.</p> 
+    </section>
+
+    <section id="about">
+      <h2>Tentang Saya</h2>
+      <p><strong>Nim:</strong> &#127380; 2511500018</p>
+      <p><strong>Nama Lengkap:</strong> Dika Yansah &#128526;</p>
+      <p><strong>Tempat lahir:</strong> Pangkalpinang &#128205;</p>
+      <p><strong>Tanggal Lahir:</strong> &#128197; 13 Desember 2006</p>
+      <p><strong>Hobi Saya:</strong> Mendengarkan musik &#127911; Bermain game &#127918;</p>
+      <p><strong>Pasangan:</strong> Belum laku &#128514;</p>
+      <p><strong>Saya berstatus:</strong> Mahasiswa &#127891;</p>
+      <p><strong>Nama ayah:</strong> Marwadi &#128104;</p>
+      <p><strong>Nama ibu:</strong> Ngatinah &#128105;</p>
+      <p><strong>Nama kakak:</strong> Indah Nuryanto &#128102;</p>
+      <p><strong>Nama Adik:</strong> Tidak ada &#128517;</p>
+    </section>
+
+
+    <section id="contact">
+      <h2>Kontak Kami</h2>
+      <form id="formKontak">
+        <label>
+          <span>Nama:</span>
+          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama">
+        </label>
+
+        <label>
+          <span>Email:</span>
+          <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email">
+        </label>
+
+        <label>
+          <span>Pesan Anda:</span>
+          <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..."></textarea>
+        </label>
+
+        <small id="charCount">0/200 karakter</small>
+        <div class="tombol">
+          <button type="submit">Kirim</button>
+          <button type="reset">Batal</button>
+        </div>
+      </form>
+    </section>
+
+    <section id="latihan">
+      <h2>Latihan JavaScript</h2>
+      <p id="pesan">Teks awal</p>
+      <button onclick="ubahTeks()">Ubah Teks</button>
+      <br><br>
+      <button id="tombol">Klik Saya</button>
+      <br><br>
+      <input type="text" id="nama" placeholder="Masukkan nama">
+      <button onclick="cekNama()">Submit</button>
+    </section>
+  </main>
+
+  <footer>
+    <p>&copy; 2025 DIKA YANSAH [2511500018] </p>
+  </footer>
+
+
+  <script>
+
+    function ubahTeks() {
+      document.getElementById("pesan").innerText = "Teks berhasil diubah!";
+    }
+
+    document.getElementById("tombol").addEventListener("click", function() {
+      alert("Tombol diklik!");
+      console.log("Tombol berhasil diklik!");
+    });
+
+    // Fungsi cek nama
+    function cekNama() {
+      let nama = document.getElementById("nama").value;
+      if (nama === "") {
+        alert("Nama tidak boleh kosong!");
+      } else {
+        alert("Halo, " + nama + "!");
+      }
+    }
+
+        document.querySelector("form").addEventListener("submit", function (e) {
+        const nama = document.getElementById("txtNama").value.trim();
+        const email = document.getElementById("txtEmail").value.trim();
+        const pesan = document.getElementById("txtPesan").value.trim();
+        if (nama === "" || email === "" || pesan === "") {
+        alert("Semua kolom wajib diisi!");
+        e.preventDefault();}
+        });
+        
+  </script>
+</body>
+</html>
+<script src="script.js"></script>
+<script>
