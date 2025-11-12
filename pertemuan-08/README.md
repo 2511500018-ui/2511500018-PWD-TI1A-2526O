52fa8804351a6918280178326895a33e1319fedd
unknown <2511500018@mahasiswa.atmaluhur.ac.id>
2025-11-12 11:40:11 +0700

    pertemuan 8

--- pertemuan-08/README.md
+++ pertemuan-08/README.md
@@ -1,1 +1,11 @@
 # pertemuan-08
+
+Hari ini, Rabu 12.NOV.2025, saya:<br>
+NIM: 2511500018<br>
+Nama: Dika Yansah<br>
+Kelompok: TI1A<br>
+<br>
+Proses saya mengerjakan UTS ini sebagai berikut:<br>
+<ol>
+  <li>silahkan diisi sesuai proses pengerjaan kalian masing-masing</li>
+</ol>
---
+++ pertemuan-08/index.php
@@ -0,0 +1,186 @@
+<?php
+session_start();
+
+$sesnama = "";
+if (isset($_SESSION["sesnama"])):
+  $sesnama = $_SESSION["sesnama"];
+endif;
+
+$sesemail = "";
+if (isset($_SESSION["sesemail"])):
+  $sesemail = $_SESSION["sesemail"];
+endif;
+
+$sespesan = "";
+if (isset($_SESSION["sespesan"])):
+  $sespesan = $_SESSION["sespesan"];
+endif;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Judul Halaman</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <h1>Ini Header</h1>
+    <button class="menu-toggle" id="menuToggle" aria-label="Toggle Navigation">
+      &#9776;
+    </button>
+    <nav>
+      <ul>
+        <li><a href="#home">Beranda</a></li>
+        <li><a href="#about">Tentang</a></li>
+        <li><a href="#form">Pengunjung</a></li>
+        <li><a href="#contact">Kontak</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <section id="home">
+      <h2>Selamat Datang</h2>
+      <?php
+      echo "halo dunia!<br>";
+      echo "nama saya dika";
+      ?>
+      <p>Ini contoh paragraf HTML.</p>
+    </section>
+
+    <section id="about">
+      <?php
+      $nim = 2511500018;
+      $NIM = '2511500018';
+      $nama = "Dika Yansah";
+      $Nama = 'Dika Yansah';
+      $tempat = "Pangkalpinang";
+      ?>
+      <h2>Tentang Saya</h2>
+      <p><strong>NIM:</strong>
+        <?php
+        echo $NIM;
+        ?>
+      </p>
+      <p><strong>Nama Lengkap:</strong>
+        <?php
+        echo $Nama;
+        ?> &#128526;
+      </p>
+      <p><strong>Tempat Lahir:</strong> <?php echo $tempat; ?></p>
+      <p><strong>Tanggal Lahir:</strong> 13 Desember 2006</p>
+      <p><strong>Hobi:</strong> Bermain Game dan Mendengarkan Musik &#127926;</p>
+      <p><strong>Pasangan:</strong> Belum ada &hearts;</p>
+      <p><strong>Pekerjaan:</strong> Mahasiswa</p>
+      <p><strong>Nama Orang Tua:</strong> Bapak Marwadi Dan Ibu Ngatinah</p>
+      <p><strong>Nama Kakak:</strong> Indah Nuryanto</p>
+      <p><strong>Nama Adik:</strong> Tidak Ada <?php echo $sespesan ?></p>
+    </section>
+
+        <section id="form">
+  <h2>Pendaftaran Profil Pengunjung</h2>
+
+  <form action="proses.php" method="POST">
+    <label>
+      <span>NIM:</span>
+      <input type="text" name="nim" placeholder="Masukkan NIM" required>
+    </label>
+
+    <label>
+      <span>Nama Lengkap:</span>
+      <input type="text" name="nama" placeholder="Masukkan nama lengkap" required>
+    </label>
+
+    <label>
+      <span>Tempat Lahir:</span>
+      <input type="text" name="tempat" placeholder="Masukkan tempat lahir" required>
+    </label>
+
+    <label>
+      <span>Tanggal Lahir:</span>
+      <input type="text" name="Tanggal" placeholder="Masukkan Tanggal Lahir" required>
+    </label>
+
+    <label>
+      <span>Hobi:</span>
+      <input type="text" name="hobi" placeholder="Masukkan hobi">
+    </label>
+
+    <label>
+      <span>Pasangan:</span>
+      <input type="text" name="pasangan" placeholder="Masukkan nama pasangan">
+    </label>
+
+    <label>
+      <span>Pekerjaan:</span>
+      <input type="text" name="pekerjaan" placeholder="Masukkan pekerjaan">
+    </label>
+
+    <label>
+      <span>Nama Orang Tua:</span>
+      <input type="text" name="ortu" placeholder="Masukkan nama orang tua">
+    </label>
+
+    <label>
+      <span>Nama Kakak:</span>
+      <input type="text" name="kakak" placeholder="Masukkan nama kakak">
+    </label>
+
+    <label>
+      <span>Nama Adik:</span>
+      <input type="text" name="adik" placeholder="Masukkan nama adik">
+    </label>
+
+      <button type="submit">Kirim</button>
+      <button type="reset">Batal</button>
+  </form>
+</section>
+
+    <section id="contact">
+      <h2>Kontak Kami</h2>
+      <form action="proses.php" method="POST">
+
+        <label for="txtNama"><span>Nama:</span>
+          <input type="text" id="txtNama" name="txtNama" placeholder="Masukkan nama" required autocomplete="name">
+        </label>
+
+        <label for="txtEmail"><span>Email:</span>
+          <input type="email" id="txtEmail" name="txtEmail" placeholder="Masukkan email" required autocomplete="email">
+        </label>
+
+        <label for="txtPesan"><span>Pesan Anda:</span>
+          <textarea id="txtPesan" name="txtPesan" rows="4" placeholder="Tulis pesan anda..." required></textarea>
+          <small id="charCount">0/200 karakter</small>
+        </label>
+
+
+        <button type="submit">Kirim</button>
+        <button type="reset">Batal</button>
+      </form>
+
+      <?php if (!empty($sesnama)): ?>
+        <br><hr>
+        <h2>Yang menghubungi kami</h2>
+        <p><strong>Nama :</strong> <?php echo $sesnama ?></p>
+        <p><strong>Email :</strong> <?php echo $sesemail ?></p>
+        <p><strong>Pesan :</strong> <?php echo $sespesan ?></p>
+      <?php endif; ?>
+
+
+
+    </section>
+  </main>
+
+  <footer>
+    <p>&copy; 2025 Dika Yansah [2511500018]</p>
+  </footer>
+
+  <script src="script.js"></script>
+</body>
+
+</html>
