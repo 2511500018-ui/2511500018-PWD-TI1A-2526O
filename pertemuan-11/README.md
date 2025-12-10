44fa60967b0bb26f726eca8dcbceba04f718b302
unknown <2511500018@mahasiswa.atmaluhur.ac.id>
2025-12-10 18:17:34 +0700

    CRUD - CREATE

--- pertemuan-11/README.md
+++ pertemuan-11/README.md
@@ -1,1 +1,20 @@
 # pertemuan-11
+
+Nama : Dika Yansah
+Nim : 2511500018 
+
+hari ini, rabu 10 desember 2025 saya mempelajari : 
+<ol>
+  <li>mempelajari CRUD - CREATE.</li>
+  <li>buat file baru dengan nama koneksi.php di dalam folder pertemuan-10.</li>
+  <li>mamsukkan kode pada koneksi.php:</li>
+  <li>membuka alamat http://localhost/phpmyadmin</li>
+  <li>membuka database, Pada isian Database name, ketik nama database yakni db_pwd2025. Klik Create.</li>
+  <li>membuka tab sql, Pada isian perintah SQL, ketikkan query untuk membuat tabel tbl_tamu yang akan menyimpan data dari form contact.</li>
+  <li>setelah mengetik query untuk membuat tabel tbl_tamu, klik Go</li>
+  <li>buat file baru dengan nama read.php di dalam folder pertemuan-10, ketikan sebuah kode agar bisa menampilkan rekord data pada tabel tbl_tamu</li>
+  <li>Agar tabel tbl_tamu memiliki data klik tab Insert</li>
+  <li>mengcopy file read.php, setelah itu merename nya menjadi read_inc.php pada pertemuan 10</li>
+  <li>memodif file index.php pada pertemuan 10 di baris 146</li>
+  <li>Hapus baris 133-141 pada file index.php pertemuan 10 karena sudah tidak membutuhkannya lagi (sudah diambil alih oleh file read_inc.php)</li>
+</ol>
---
+++ pertemuan-11/tbl_tamu.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 10 Des 2025 pada 11.14
+-- Versi server: 5.7.33
+-- Versi PHP: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tamu`
+--
+
+CREATE TABLE `tbl_tamu` (
+  `cid` int(11) NOT NULL,
+  `cnama` varchar(100) DEFAULT NULL,
+  `cemail` varchar(100) DEFAULT NULL,
+  `cpesan` text
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `tbl_tamu`
+--
+
+INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`) VALUES
+(1, 'DIKA YANSAH', '2511500018@mahasiswa.atmaluhur.ac.id', 'GGMU, ora dukung MU ora gacor'),
+(2, 'hamak', 'hamak@gmail.com', 'GLORY GLORY MAN UNITED'),
+(3, 'udin', 'udinganteng@gmail.com', 'Kenapa kalau duduk di atas motor bawaannya pengen ketawa ? karena duduk di atas jokes xixixi'),
+(4, 'abdul', 'abdulkece@gmail.com', 'Makanan, makanan apa yang mudah dibuat ? TAHU EASY xixixixi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
